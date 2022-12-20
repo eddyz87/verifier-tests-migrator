676ec8da413ae1a669acb7cbe059651c36ec029e
Eduard Zingerman <eddyz87@gmail.com>
2022-12-20 12:59:41 +0200

    renamed compile-c-grammar.py to build.py

--- build.py
+++ build.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3
+
 from tree_sitter import Language
 
 def build_grammar():
     Language.build_library('build/my-languages.so', ['tree-sitter-c'])
+
+if __name__ == '__main__':
+    build_grammar()
